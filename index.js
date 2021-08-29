37940dbfbe7b5ca164342d270d0dcbcac7e73d7a
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2021-08-29 10:41:02 -0400

    First http web server

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var _ = require('underscore');
+const express = require('express')
+const app = express();
 
-var result = _.contains([1,2,3],2);
-console.log(result);
+app.get('/', (req,res)=> {
+    res.send("Hello World");
+});
+
+app.get('/api/courses', (req,res) => {
+    res.send([1,2,3,4,5]);
+});
+
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
